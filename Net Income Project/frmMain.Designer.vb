1e51415993f48829bd5fc5e270798244100b22e8
Tom Dickelman <tsdickelman@gmail.com>
2022-08-15 16:18:08 -0500

    Add files via upload

---
+++ Net Income Project/frmMain.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblNetIncome = New System.Windows.Forms.Label()
+        Me.txtRevenue = New System.Windows.Forms.TextBox()
+        Me.txtExpenses = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Net_Income_Project.My.Resources.Resources.EdwardsAndSon
+        Me.PictureBox1.Location = New System.Drawing.Point(59, 14)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(314, 64)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblNetIncome
+        '
+        Me.lblNetIncome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNetIncome.Location = New System.Drawing.Point(300, 142)
+        Me.lblNetIncome.Name = "lblNetIncome"
+        Me.lblNetIncome.Size = New System.Drawing.Size(120, 32)
+        Me.lblNetIncome.TabIndex = 6
+        '
+        'txtRevenue
+        '
+        Me.txtRevenue.Location = New System.Drawing.Point(33, 142)
+        Me.txtRevenue.Name = "txtRevenue"
+        Me.txtRevenue.Size = New System.Drawing.Size(126, 32)
+        Me.txtRevenue.TabIndex = 1
+        '
+        'txtExpenses
+        '
+        Me.txtExpenses.Location = New System.Drawing.Point(169, 142)
+        Me.txtExpenses.Name = "txtExpenses"
+        Me.txtExpenses.Size = New System.Drawing.Size(125, 32)
+        Me.txtExpenses.TabIndex = 3
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(90, 208)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(102, 51)
+        Me.btnCalc.TabIndex = 4
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(237, 208)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(102, 51)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 112)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(132, 25)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Sales &revenue:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(164, 112)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(93, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "&Expenses:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(295, 112)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(113, 25)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Net Income:"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(432, 286)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.txtExpenses)
+        Me.Controls.Add(Me.txtRevenue)
+        Me.Controls.Add(Me.lblNetIncome)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "0"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents lblNetIncome As Label
+    Friend WithEvents txtRevenue As TextBox
+    Friend WithEvents txtExpenses As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Net Income Project/frmMain.vb
@@ -0,0 +1,42 @@
+﻿Public Class frmMain
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblNetIncome.Click
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub txtRevenue_TextChanged(sender As Object, e As EventArgs) Handles txtRevenue.TextChanged
+
+    End Sub
+
+    Private Sub txtExpenses_TextChanged(sender As Object, e As EventArgs) Handles txtExpenses.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+End Class
